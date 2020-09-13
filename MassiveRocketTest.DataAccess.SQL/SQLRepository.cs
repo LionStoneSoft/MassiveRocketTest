58e567a2b82d8915b7fd61bb09f04a8acc604310
Lion Stone <44401494+LionStoneSoft@users.noreply.github.com>
2020-09-13 13:25:47 +0100

    Website now communicates with SQL database rather than internal

---
+++ MassiveRocketTest.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,59 @@
+﻿using MassiveRocketTest.Core.Contracts;
+using MassiveRocketTest.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveRocketTest.DataAccess.SQL
+{
+    public class SQLRepository<P> : IRepository<P> where P : BaseEntity
+    {
+
+        internal DataContext context;
+        internal DbSet<P> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<P>();
+        }
+
+        public IQueryable<P> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var p = Find(Id);
+            if (context.Entry(p).State == EntityState.Detached)
+                dbSet.Attach(p);
+
+            dbSet.Remove(p);
+        }
+
+        public P Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
+
+        public void Insert(P p)
+        {
+            dbSet.Add(p);
+        }
+
+        public void Update(P p)
+        {
+            dbSet.Attach(p);
+            context.Entry(p).State = EntityState.Modified;
+        }
+    }
+}
--- MassiveRocketTest.WebUI/App_Start/UnityConfig.cs
+++ MassiveRocketTest.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MassiveRocketTest.Core.Contracts;
 using MassiveRocketTest.Core.Models;
 using MassiveRocketTest.DataAccess.InMemory;
+using MassiveRocketTest.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MassiveRocketTest.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
